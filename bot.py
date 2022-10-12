37ea57292432112a35f0c84a9583b3b879ae3c4b
pepsivstim <pepsivstim@gmail.com>
2022-10-12 17:03:57 -0400

    updated bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,26 @@
 from selenium import webdriver
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import pandas as pd
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.youtube.com/playlist?list=PLcysmcZSZqKQ5FAiCQlO_kOoSSNcsjBgK")
+products=[] #List to store name of the product
+prices=[] #List to store price of the product
+ratings=[] #List to store rating of the product
+
+content = driver.page_source
+#print(content)
+soup = BeautifulSoup(content, features="html.parser")
+#print(soup.prettify())
+
+found = "index"
+i = 0
+for link in soup.find_all('a'):
+
+    i = i + 1
+    if found in link:
+        print(link.get('href'))
+        print(i)
